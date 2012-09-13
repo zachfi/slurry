13d78361af2ebb187770b5573ffa325d0ebab42b
Zach Leslie <zach@puppetlabs.com>
2012-09-13 08:34:27 -0700

    move graphite class into its own file

--- lib/slurry.rb
+++ lib/slurry.rb
@@ -1,3 +1,5 @@
+require 'slurry/graphite'
+
 require 'json'
 require "redis"
 require 'json2graphite'
@@ -6,35 +8,6 @@ require 'json2graphite'
 #
 module Slurry
   module_function
-
-  # Handles connection details to the graphite server.
-  class Graphite
-
-    # Opens a socket with the specified graphite server.
-    #
-    # @param [String] server
-    # @param [String] port
-    def initialize(server,port)
-      @server, @port = server, port
-      @s = TCPSocket.open(server,port)
-    end
-
-    # Puts the graphite formatted string into the open socket.
-    #
-    # @param [String] target the graphite formatted target in dotted notion
-    # @param [String] value the value of the target
-    # @param [String] time the time that the sample was taken
-    def send (target,value,time)
-      line = [target,value,time].join(" ")
-      @s.puts(line)
-    end
-
-    # Close the open socket to the graphite server.
-    def close
-      @s.close
-    end
-
-  end
 
 
   # Wraps received hash in new hash with timestamp applied.
