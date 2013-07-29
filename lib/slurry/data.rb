a9fc1082e403cc96cb13eb6f3431a7c66bb79bcb
Zach Leslie <zach@puppetlabs.com>
2013-07-28 22:40:59 -0700

    begin breaking out data into its own lib

---
+++ lib/slurry/data.rb
@@ -0,0 +1,46 @@
+require 'slurry'
+
+module Slurry
+  class Data
+    # Data is a hash
+    def initialize(initdata)
+      @initdata = initdata
+      @data = Hash.new
+      encap!
+    end
+
+    private
+
+    #
+    # Encapsulate the data in a new hash under the key :data and
+    # adds a timestamp to the new hash at key :time
+    #
+    def encap!
+      unless proper?
+        timestamp
+        @data[:data] = hash
+      end
+    end
+
+    def decap
+      @data[:data]
+    end
+
+    # Wraps received hash in new hash with timestamp applied.
+    #
+    # @param [Hash] hash
+    # @parah [Int] time time of the recorded event
+    #
+    def timestamp (time=Time.now.to_i)
+      @data[:time] = time
+    end
+
+    # Check to see if the data hash is in its correct state for
+    # storage.  Returns bool
+    #
+    def proper?
+      @data and @data[:time] and @data[:hash]
+    end
+
+  end
+end
